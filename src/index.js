568dbdcc844b1cde1a425e7394424a640d10c5ae
Ramon Moraes <ramon.moraes94@gmail.com>
2018-06-29 00:23:48 -0300

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import CardSnippet from './components/CardSnippet';
+import CardSnippet from "./components/CardSnippet";
 
 const appendToFeed = ele => {
   const feed = document.querySelector(".AppShell-feed");
@@ -6,14 +6,20 @@ const appendToFeed = ele => {
 };
 
 const mockData = [
-    {
-        title: "Big title",
-        paragraph: "Not so big paragraph",
-    },
-    {
-        title: "Sad title =(",
-        paragraph: ":,( .....",
-    }
+  {
+    title: "Big title",
+    paragraph: "Not so big paragraph"
+  },
+  {
+    title: "Sad title =(",
+    paragraph: ":,( ....."
+  },
+  {
+    title: "Olha eu com boné",
+    paragraph: "Olha eu sem boné =(",
+    source: "https://imgcentauro-a.akamaihd.net/180x180/91317402.jpg",
+    imgSrc: "https://imgcentauro-a.akamaihd.net/180x180/91317402.jpg"
+  }
 ];
 
 mockData.forEach(card => appendToFeed(CardSnippet(card)));
