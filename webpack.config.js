bf0abc862e04666892958c10001bad8dd4cdee16
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-04 20:03:16 -0300

    Adding html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -21,5 +22,11 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'NewsFeed',
+      template: './src/index.html'
+    })
+  ]
 };
