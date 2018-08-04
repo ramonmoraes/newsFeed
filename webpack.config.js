7181e50c8afd2c4c1b7038c7c056e25650b2104d
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-04 19:43:41 -0300

    Getting two files, one for bundle.js other for service-worker.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: {
       bundle: './src/index.js',
-      sw: './src/serviceWorker/service-worker.js',
+      sw: './src/serviceWorker/register.js',
     },
   output: {
     path: path.resolve(__dirname, 'dist'),
