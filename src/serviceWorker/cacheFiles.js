e1917e35bc25cc27dd2acdf856be135e47ed482d
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-05 22:40:48 -0300

    initial cacheFiles.js

--- src/serviceWorker/cacheFiles.js
+++ src/serviceWorker/cacheFiles.js
@@ -1,10 +1,16 @@
-const cachedFiles = [
-  'bundle.js',
-  'style.css'
-];
+const cacheName = "newsFeed";
+const cachedFiles = ["index.html", "bundle.js", "style.css"];
 
 self.addEventListener("install", ev => {
-  ev.waitUntil(
-    caches.open("newsFeed").then(cache => cache.addAll(cachedFiles))
+  ev.waitUntil(caches.open(cacheName).then(cache => cache.addAll(cachedFiles)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
+
+
