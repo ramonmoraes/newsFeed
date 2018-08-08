a35cf6ad15925311f4dab9d2ea73f3ea0fdf52fd
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-07 21:34:00 -0300

    add active event listener for test

--- src/serviceWorker/cacheFiles.js
+++ src/serviceWorker/cacheFiles.js
@@ -5,6 +5,10 @@ self.addEventListener("install", ev => {
   ev.waitUntil(caches.open(cacheName).then(cache => cache.addAll(cachedFiles)));
 });
 
+self.addEventListener('activate', event => {
+  console.log('V1 now ready to handle fetches!');
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
